2903533a134f1e0bcd3d81c43b22e235478a919f
zevav <zev@averba.ch>
2018-10-23 14:18:06 -0400

    more tests, more features

--- functional_tests.py
+++ functional_tests.py
@@ -1,25 +1,33 @@
 from selenium import webdriver
 d = webdriver.Firefox()
-#User goes to the page.
+# User goes to the page.
 d.get('http://0.0.0.0:8000/index.html')
 
-#Users browser displays title "Vocabulary through Culture"
+# Users browser displays title "Vocabulary through Culture"
 assert "Vocabulary through Culture" in d.title
 
-#User sees text: "Welcome to Vocabulary through Culture!"
-print (d.page_source)
+# User sees text: "Welcome to Vocabulary through Culture!"
 body = d.find_element_by_tag_name('body')
 assert "Vocabulary through Culture" in body.text
 
-#User sees mail field and password field labeled "e-mail: " and "Password: " and submit button
+# User sees a form for registering
+form = d.find_element_by_id('register-form')
+
+# User sees mail and password text fields
 mail_field = d.find_element_by_css_selector('input#mail-id')
-assert mail_field.get_attribute('type')=='text'
-print(dir(mail_field))
+assert mail_field.get_attribute('type') == 'text'
+password_field = d.find_element_by_css_selector('input#password')
+assert password_field.get_attribute('type') == 'text'
 
+# User sees labels for those text fields
+print(form.text)
+assert 'Email:' in form.text
+assert 'Password:' in form.text
 
+# User enters username "John@gmail.com", password "Johnspassword" and clicks the submit button.
+submit_button = d.find_element_by_css_selector(
+    'input[value="Create Account!"]')
+submit_button.click()
 
-#User enters username "John@gmail.com", password "Johnspassword" and clicks the submit button.
-
-#Thw browser takes John to a new webpage: Title "Welcome"
-
-
+# Thw browser takes John to a New webpage: Title "Welcome"
+assert d.title == 'Welcome'
