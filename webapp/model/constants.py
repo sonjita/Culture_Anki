576fbe0167a853d9ac42e7d0b9c4fc416d54d19d
sonjita <sonjaleaheinze@gmail.com>
2018-12-10 02:33:10 -0500

    Started defining db related constants

---
+++ webapp/model/constants.py
@@ -0,0 +1,2 @@
+MEDIA_TYPES = ("Book", "Movie", "Series", "Song")
+LANGUAGES = {"English":["nouns", "regular verbs", "irregular verbs", "contractions"], "Spanish":["nouns","adjectives","regular verbs", "irregular verbs"]} # FIXME
